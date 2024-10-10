d61eccce09deb2f79e818644f0b1557a3da43b47
Seongyun <okks3579@gmail.com>
2024-10-10 22:36:29 +0900

    업로드

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int[] arr = new int[N];
-
-        for (int i =0; i<N ;i++) {
-            arr[i]=0;
-        }
-        for (int q = 0 ; q<M ;q++) {
-            st = new StringTokenizer(br.readLine());
-            int i = Integer.parseInt(st.nextToken());
-            int j = Integer.parseInt(st.nextToken());
-            int k = Integer.parseInt(st.nextToken());
-
-            for (int a = i-1; a<j; a++ ) {
-                arr[a]=k;
+        char[][] arr = new char[5][15];
+        int max = 0;
+        for (int i =0;i<5;i++) {
+            String str = br.readLine();
+            if (str.length()>max) max = str.length();
+            for (int j=0;j<str.length();j++) {
+                arr[i][j]=str.charAt(j);
             }
         }
-        StringBuilder sb = new StringBuilder();
-        for (int i =0 ; i<N ; i++) {
-            sb.append(arr[i]);
-            sb.append(" ");
+        for (int i=0;i<max;i++) {
+            for (int j=0;j<5;j++) {
+                if(arr[j][i]=='\0')
+                    continue;
+                System.out.print(arr[j][i]);
+            }
         }
-        System.out.println(sb);
+
     }
 }
